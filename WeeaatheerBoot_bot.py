1b3bd706fa8a3294a094eefa5c3c54e23d38dd85
Матфей Лисов Игоревич <lmatfey11@gmail.com>
2024-11-22 10:31:16 +0300

    API

--- WeeaatheerBoot_bot.py
+++ WeeaatheerBoot_bot.py
@@ -7,8 +7,8 @@ import json  # Для сохранения и загрузки данных
 logging.basicConfig(level=logging.INFO)
 
 # Токен Telegram-бота и OpenWeather API
-TELEGRAM_API_TOKEN = "7594507179:AAGRbNwpTE08yRSCx8L_fDtyb5fBo-elUBk"  # Замените на ваш токен
-WEATHER_API_KEY = "eeaea6e6965d4b11c6da0f3f98eb4f80"  # Замените на ваш API-ключ
+TELEGRAM_API_TOKEN = "7594507179:AAGRbNwpTE08yRSCx8L_fDtyb5fBo-elUBk"  # Тг-токен
+WEATHER_API_KEY = "eeaea6e6965d4b11c6da0f3f98eb4f80"  # API-ключ
 
 # Инициализация бота
 bot = telebot.TeleBot(TELEGRAM_API_TOKEN)
